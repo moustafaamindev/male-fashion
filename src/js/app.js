03b15ac6ad6b0c94b101d68155895dc9d90f00e3
israa burhan <israaburhan97@gmail.com>
2023-03-30 07:14:13 +0300

    Add files project

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import "../scss/app.scss";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+
+import "bootstrap/dist/js/bootstrap.bundle";
+
+const home = require("./home");
---
+++ webpack.config.js
@@ -0,0 +1,196 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const webpack = require("webpack");
+const path = require("path");
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src", "js", "app.js"),
+  },
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dev"),
+    filename: "my-first-webpack.js",
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  target: ["web", "es5"],
+
+  devServer: {
+    port: 8000,
+    open: true,
+    hot: false,
+    static: {
+      directory: path.join(__dirname, "dev"),
+    },
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({ exclude: /node_modules/ }), new CssMinimizerPlugin()],
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [autoprefixer()],
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/index.ejs",
+      filename: "index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/about.ejs",
+      filename: "about.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/blog-details.ejs",
+      filename: "blog-details.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/blog.ejs",
+      filename: "blog.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/check-out.ejs",
+      filename: "check-out.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/contacts.ejs",
+      filename: "contacts.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/pages.ejs",
+      filename: "pages.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/shop-details.ejs",
+      filename: "shop-details.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/shop.ejs",
+      filename: "shop.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      inject: "body",
+      template: "./src/shopping-cart.ejs",
+      filename: "shopping-cart.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/i,
+        use: ["html-loader", "template-ejs-loader"],
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|gif|jpg|svg)$/i,
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};

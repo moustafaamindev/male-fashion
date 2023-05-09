06167969345c41d444884ec1c03e737ff3463159
Mostafa Amin <125675410+moustafaamindev@users.noreply.github.com>
2023-05-10 01:42:33 +0300

    page loader

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,8 @@ const blog = require("./blog");
 const shopDetails = require("./shop-details");
 const shop = require("./shop");
 const home = require("./home");
+
+let loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    loader.classList.add("spinner");
+});
